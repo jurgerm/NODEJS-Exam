0e1f093635be24009f640231859f9c5b748af1c7
Jurgita Germanavičienė <jurgita.germanaviciene@codeacademylt.onmicrosoft.com>
2021-12-22 00:04:51 +0200

    Pakeista duomenų struktūra

--- nodejs-exam-backend/sql/1. Create_table_exam_users.sql
+++ nodejs-exam-backend/sql/1. Create_table_exam_users.sql
@@ -2,7 +2,7 @@ create table if not exists exam_users(
   id INTEGER AUTO_INCREMENT NOT NULL,
   full_name VARCHAR (50) NOT NULL,
   email VARCHAR (50) NOT NULL,
-  password CHAR (60) NOT NULL,
+  password CHAR (255) NOT NULL,
   reg_timestamp INTEGER,
   PRIMARY KEY (id),
   UNIQUE (email)
